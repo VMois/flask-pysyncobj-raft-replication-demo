8cd72d4333662c163130f8b9ae6882dde5fe05f8
VMois <vmois@protonmail.com>
2021-09-13 22:21:54 +0200

    remove FIFO scheduler and create IOHeavyScheduler, vary response time randomly according to Erlang distribution

--- flask_raft/scheduler.py
+++ flask_raft/scheduler.py
@@ -1,3 +1,4 @@
+import random
 import logging
 import time
 from typing import Dict
@@ -6,31 +7,37 @@ from abstract import Scheduler
 from raft import TASKS_QUEUE, is_leader
 
 
-class FifoScheduler(Scheduler):
-    SLEEP_BETWEEN_TASKS_SECONDS = 0.1
+class IOHeavyScheduler(Scheduler):
+    SLEEP_BETWEEN_TASKS_SECONDS = 0.05
 
     def startup(self) -> None:
-        logging.info('FIFO scheduler starts')
+        logging.info('IOHeavyScheduler starts')
 
     def shutdown(self) -> None:
-        logging.info('FIFO scheduler stops')
+        logging.info('IOHeavyScheduler stops')
 
     @staticmethod
-    def _process_task(task: Dict) -> None:
-        logging.info(f'Scheduler processing task: {task}')
-        time.sleep(1)
+    def _erlang_distribution_approximation(k: float = 2, gamma: float = 2) -> float:
+        return random.gammavariate(k, 1 / gamma)
+
+    def _simulate_send_task(self, task: Dict) -> None:
+        logging.debug(f'IOHeavyScheduler sends task: {task}')
+        io_will_take_seconds = self._erlang_distribution_approximation() / task.get('divider', 2)
+        time.sleep(io_will_take_seconds)
 
     def handle(self) -> None:
         if TASKS_QUEUE.empty() or not is_leader():
             time.sleep(self.SLEEP_BETWEEN_TASKS_SECONDS)
             return
 
+        # TODO: this is blocking and needs sync of the cluster
         task = TASKS_QUEUE.get(sync=True)
-        self._process_task(task)
+
+        self._simulate_send_task(task)
 
 
 class SchedulerFactory:
     @staticmethod
-    def create_scheduler(scheduler_type: str = 'fifo') -> Scheduler:
-        if scheduler_type == 'fifo':
-            return FifoScheduler()
+    def create_scheduler(scheduler_type: str = 'io') -> Scheduler:
+        if scheduler_type == 'io':
+            return IOHeavyScheduler()
